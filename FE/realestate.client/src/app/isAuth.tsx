f793e357f5f18682dcb0898bc5248ae6e7b0eab2
Nguyễn Thế Vinh <nguyenthevinhmt0x@gmail.com>
2024-02-19 17:35:16 +0700

    set up redux persist

--- FE/realestate.client/src/app/isAuth.tsx
+++ FE/realestate.client/src/app/isAuth.tsx
@@ -1,20 +1,30 @@
 "use client";
-import { useEffect } from "react";
-import { redirect } from "next/navigation";
-import { CookieService } from "../shared/services/cookies.service";
 import React from "react";
-import { GetServerSidePropsContext } from "next";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import { useRouter } from "next/navigation";
 
 export default function isAuth(Component: any) {
+  
   return function IsAuth(props: any) {
-    // const auth = CookieService.getAccessToken();
-    // console.log("cookie", auth);
-    const { token, context, message } = props; // Access token from props passed by getServerSideProps
-
-    if (!token) {
-      console.log("không có token", message);
-      return null; // If token is not present, render nothing (already redirected)
-    }
+    const authSelector = useSelector((state:RootState) => {
+      state.auth
+    });
+    const router = useRouter();
+    console.log("authSelector", (authSelector as any)?.data)
+    // if(authSelector){
+    // }
+    // const repo = await getServerSideProps()
+    // console.log(repo)
+    // repo.then((data) => {
+    //   return data
+    // });
+    // const {props} = repo
+    // if (!data) {
+      // console.log("không có token", token);
+    //   return null;
+    // }
     // useEffect(() => {
     //   if (!auth) {
     //     console.log("Hàm này chạy rồi");
@@ -28,21 +38,9 @@ export default function isAuth(Component: any) {
     return <Component {...props} />;
   };
 }
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { req } = context;
-  console.log("This is a log from getServerSideProps");
-  console.log("req", req);
-  const token1 = CookieService.getAccessToken();
-  // Lấy cookie từ request
-  const token = req.cookies.access_token;
-  console.log("token1", token1);
-
-  // Trả về props với token để sử dụng trong component
-  return {
-    props: {
-      token,
-      context,
-      message: `Next.js is awesome`,
-    },
-  };
+async function getServerSideProps()
+{
+  const res = await axios.get('api/get-cookie');
+  console.log(res)
+  return res.data
 }
