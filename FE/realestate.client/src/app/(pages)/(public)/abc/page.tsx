f7fd16178292ebecdffe6c7011bd3ba6b5bae9a7
Nguyễn Thế Vinh <81474434+nguyenthevinhmt@users.noreply.github.com>
2024-02-25 13:14:24 +0700

    refresh token ok

---
+++ FE/realestate.client/src/app/(pages)/(public)/abc/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import useCountStore from "@/app/store";
+
+const CountPage = () => {
+  const { count, increment, decrement } = useCountStore();
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default CountPage;
