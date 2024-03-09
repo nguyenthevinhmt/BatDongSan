93799191a09252766f63627f96d631dca4889a37
Nguyễn Thế Vinh <81474434+nguyenthevinhmt@users.noreply.github.com>
2024-03-09 16:17:35 +0700

    api tinh thanh pho

---
+++ BE/RealEstate.Server/Services/RealEstate.Domain/Entities/Provinces.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Domain.Entities
+{
+    /// <summary>
+    /// Tỉnh/Thành phố
+    /// </summary>
+    public class Provinces
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        /// <summary>
+        /// Code
+        /// </summary>
+        [MaxLength(20)]
+        public string Code { get; set; } = null!;
+        /// <summary>
+        /// Tên tỉnh/thành phố
+        /// </summary>
+        [MaxLength(256)]
+        public string Name { get; set; } = null!;
+        /// <summary>
+        /// Tên tiếng anh
+        /// </summary>
+        [MaxLength(256)]
+        public string? NameEn { get; set; }
+        /// <summary>
+        /// Tên đầy đủ
+        /// </summary>
+        [MaxLength(256)]
+        public string FullName { get; set; } = null!;
+        /// <summary>
+        /// Tên đầy đủ tiếng anh
+        /// </summary>
+        [MaxLength(256)]
+        public string? FullNameEn { get; set; }
+        /// <summary>
+        /// Tên mã code
+        /// </summary>
+        [MaxLength(256)]
+        public string? CodeName { get; set; }
+        /// <summary>
+        /// ID vùng
+        /// </summary>
+        public int? AdministrativeUnitId { get; set; }
+        /// <summary>
+        /// ID loại thành phố
+        /// </summary>
+        public int? AdministrativeRegionId { get; set; }
+    }
+}
