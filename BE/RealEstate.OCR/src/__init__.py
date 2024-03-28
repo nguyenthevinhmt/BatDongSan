6b4ad018c577acc855f23a91a2aea8105325528d
Admin <Admin@MSI>
2024-03-28 10:19:45 +0700

    fix OCR

--- BE/RealEstate.OCR/src/__init__.py
+++ BE/RealEstate.OCR/src/__init__.py
@@ -1,3 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 from src.executor import main
